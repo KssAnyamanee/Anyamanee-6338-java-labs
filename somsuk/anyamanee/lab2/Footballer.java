14e7e6fe19337907f3e3daccb8c343d5c973ba53
KssAnyamanee <anyamanee.s@kkumail.come>
2022-12-09 11:30:13 +0700

    footballer

---
+++ somsuk/anyamanee/lab2/Footballer.java
@@ -0,0 +1,33 @@
+package somsuk.anyamanee.lab2;
+
+
+public class Footballer {
+    public static void main(String[] args) {
+
+        if (args.length == 3){
+
+         String footballer_name = args[0]; //รับค่าชื่อนักฟุตบอล
+         String nationality = args[1]; //รับค่าสัญชาติ
+         String club_name = args[2]; //รับค่าทีมที่นักฟุตบอลที่ผู้เล่นคนนั้นอยู่
+         System.out.println("my favorite football player is "+ footballer_name); 
+         System.out.println("His nationallity is" +nationality);
+         System.out.println("He plays for " + club_name);
+        }
+
+        else {
+            System.out.println("Footballer <footballer name> <nationality> <club name>");
+        }
+        
+
+         
+        
+        System.out.println();
+
+
+
+
+
+
+    }
+
+}
